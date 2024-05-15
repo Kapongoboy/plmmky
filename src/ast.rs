342d3a3241005d857a072581e35852c442a3a1da
Plamedi Luis <plamedi.luis11@gmail.com>
2024-05-15 20:58:35 +0200

    fix(ast): impls for debugs an accessors

--- src/ast.rs
+++ src/ast.rs
@@ -1,19 +1,42 @@
 use crate::token::Token;
+use std::cmp::PartialEq;
+use std::fmt::{Debug, Formatter, Result};
 
-trait Node {
+pub trait Node {
     fn token_literal(&self) -> String;
 }
 
-trait Statement: Node {
+pub trait Statement: Node {
     fn statement_node(&self);
 }
 
-trait Expression: Node {
+pub trait Expression: Node {
     fn expression_node(&self);
 }
 
 pub struct Program {
-    statements: Vec<Box<dyn Statement>>,
+    pub statements: Vec<Box<dyn Statement>>,
+}
+
+impl Debug for Program {
+    fn fmt(&self, f: &mut Formatter) -> Result {
+        let mut s = String::new();
+        for stmt in &self.statements {
+            s.push_str(&stmt.token_literal());
+        }
+        write!(f, "{}", s)
+    }
+}
+
+impl PartialEq for Program {
+    fn eq(&self, other: &Self) -> bool {
+        for (left, right) in self.statements.iter().zip(other.statements.iter()) {
+            if left.token_literal() != right.token_literal() {
+                return false;
+            }
+        }
+        true
+    }
 }
 
 impl Program {
@@ -25,15 +48,39 @@ impl Program {
     }
 }
 
-struct Identifier<'a> {
+pub struct Identifier<'a> {
     token: Token<'a>,
-    // value: String, This is probably not needed seeing as we have a string inside our Ident enum
+    value: String,
 }
 
-struct LetStatement<'a> {
+impl Identifier<'_> {
+    pub fn token(&self) -> &Token {
+        &self.token
+    }
+
+    pub fn value(&self) -> &String {
+        &self.value
+    }
+}
+
+pub struct LetStatement<'a> {
     token: Token<'a>,
     name: &'a Identifier<'a>,
     value: Box<dyn Expression>,
+}
+
+impl<'a> LetStatement<'a> {
+    pub fn token(&self) -> &Token<'a> {
+        &self.token
+    }
+
+    pub fn name(&self) -> &Identifier<'a> {
+        self.name
+    }
+
+    pub fn value(&self) -> &Box<dyn Expression> {
+        &self.value
+    }
 }
 
 impl<'a> Node for LetStatement<'a> {
