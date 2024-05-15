1fdd68a4da973f451c9e04df66a2301761afb393
Plamedi Luis <plamedi.luis11@gmail.com>
2024-05-15 19:12:35 +0200

    feat(ast): core primitives implemented

--- src/ast.rs
+++ src/ast.rs
@@ -1,3 +1,5 @@
+use crate::token::Token;
+
 trait Node {
     fn token_literal(&self) -> String;
 }
@@ -21,4 +23,35 @@ impl Program {
             _ => self.statements[0].token_literal(),
         }
     }
+}
+
+struct Identifier<'a> {
+    token: Token<'a>,
+    // value: String, This is probably not needed seeing as we have a string inside our Ident enum
+}
+
+struct LetStatement<'a> {
+    token: Token<'a>,
+    name: &'a Identifier<'a>,
+    value: Box<dyn Expression>
+}
+
+impl <'a> Node for LetStatement<'a> {
+    fn token_literal(&self) -> String {
+        self.token.literal.clone()
+    }
+}
+
+impl<'a> Statement for LetStatement<'a> {
+    fn statement_node(&self) {}
+}
+
+impl<'a> Node for Identifier<'a> {
+    fn token_literal(&self) -> String {
+        self.token.literal.clone()
+    }
+}
+
+impl<'a> Expression for Identifier<'a> {
+    fn expression_node(&self) {}
 }
