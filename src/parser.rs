a34a99cc3e5a8e8a5a413d75b50e3f8fcfd54fa3
Plamedi Luis <plamedi.luis11@gmail.com>
2024-05-16 21:24:54 +0200

    fix(parser): parser runs now

--- src/parser.rs
+++ src/parser.rs
@@ -1,15 +1,16 @@
 use crate::ast;
 use crate::lexer::Lexer;
 use crate::token::{Token, TokenKind};
+use std::any::Any;
 
 pub struct Parser<'a> {
-    lex: &'a mut Lexer<'a>,
+    lex: Lexer<'a>,
     cur_token: Token<'a>,
     peek_token: Token<'a>,
 }
 
 impl<'a> Parser<'a> {
-    pub fn new(lex: &'a mut Lexer<'a>) -> Parser {
+    pub fn new(lex: Lexer<'a>) -> Parser {
         let mut p = Parser {
             lex,
             cur_token: Token::new(TokenKind::EOF, None),
@@ -30,7 +31,10 @@ impl<'a> Parser<'a> {
     }
 
     fn peek_token_is(&self, t: TokenKind) -> bool {
-        self.peek_token.ttype == t
+        match t {
+            TokenKind::IDENT(_) => matches!(self.peek_token.ttype, TokenKind::IDENT(_)),
+            _ => self.peek_token.ttype == t,
+        }
     }
 
     fn expect_peek(&mut self, t: TokenKind) -> bool {
@@ -44,23 +48,32 @@ impl<'a> Parser<'a> {
     }
 
     fn parse_let_statement(&mut self) -> Option<ast::LetStatement<'a>> {
-        let old_cur_token = self.cur_token.clone();
+        let mut stmt = ast::LetStatement::new(self.cur_token.clone(), None, None);
 
         if !self.expect_peek(TokenKind::IDENT(String::from("something"))) {
             return None;
         }
 
-        let id = ast::Identifier::new(self.cur_token.clone(), self.cur_token.literal.clone());
-        let stmt = ast::LetStatement::new(old_cur_token, Some(id), None);
+        stmt.change_name(ast::Identifier::new(
+            self.cur_token.clone(),
+            self.cur_token.literal.clone(),
+        ));
 
         if !self.expect_peek(TokenKind::ASSIGN) {
             return None;
         }
 
+        while !self.cur_token_is(TokenKind::SEMICOLON) {
+            self.next_token();
+        }
+
         Some(stmt)
     }
 
-    fn parse_statement(&mut self) -> Option<Box<dyn ast::Statement + 'a>> {
+    fn parse_statement<'b>(&mut self) -> Option<Box<dyn ast::Statement + 'b>>
+    where
+        'a: 'b,
+    {
         match self.cur_token.ttype {
             TokenKind::LET => {
                 let stmt = self.parse_let_statement();
@@ -73,7 +86,7 @@ impl<'a> Parser<'a> {
         }
     }
 
-    pub fn parse_program(&mut self) -> Option<ast::Program> {
+    pub fn parse_program(&mut self) -> Option<ast::Program<'a>> {
         let mut program = ast::Program { statements: vec![] };
 
         while self.cur_token.ttype != TokenKind::EOF {
@@ -97,7 +110,16 @@ mod tests {
     use super::*;
 
     fn test_let_statement(s: &Box<dyn ast::Statement>, test_name: &str) {
-        assert_eq!(s.token_literal(), test_name);
+        let s_any = s as &dyn Any;
+        match s_any.downcast_ref::<ast::LetStatement>() {
+            Some(stmt) => {
+                assert_eq!(
+                    stmt.name().expect("the identifier is empty").value(),
+                    test_name
+                );
+            }
+            None => panic!("expected LetStatement, got {:?}", &s.token_literal()),
+        }
     }
 
     #[test]
@@ -106,8 +128,8 @@ mod tests {
         let y = 10;\n\
         let foobar = 838383;";
 
-        let mut l = Lexer::new(input, true, None);
-        let mut p = Parser::new(&mut l);
+        let l = Lexer::new(input, true, None);
+        let mut p = Parser::new(l);
 
         let program = p.parse_program();
 
